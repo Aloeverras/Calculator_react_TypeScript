180d0c99d56614d1f37ba5324b2e9318a6c21813
Aloeverras <edouard.bouche@gmail.com>
2025-06-15 18:10:58 +0000

    modif frontend

---
+++ frontend/src/components/Calculator.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+import type { PropsCalculator } from "../interfaces/PropsCalculator";
+
+
+
+const Calculator : React.FC<PropsCalculator> = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Calculator;
