2f5d789fa6e77b044070b50657884382fb2e79c4
Aloeverras <edouard.bouche@gmail.com>
2025-06-17 09:28:42 +0000

    2025_06_17

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,9 @@ import "./scss/app.scss"
 function App() {
 
   return (
-    <div className="w-100 d-flex justify-content-center align-items-center flex-column">
-      <h1>Calculator</h1>
+    <>
       <Calculator/>
-    </div>
+    </>
   )
 }
 
--- frontend/src/components/Calculator.tsx
+++ frontend/src/components/Calculator.tsx
@@ -1,16 +1,15 @@
 import type React from "react";
 import type { PropsCalculator } from "../interfaces/PropsCalculator";
-import KeyBoard from "./KeyBoard";
-
 
 
 const Calculator : React.FC<PropsCalculator> = () => {
     return (
-        <main>
-            <KeyBoard buttonsBoard={
-                ["AC", "/", "1", "2", "3", "*", "4", "5", "6", "+", "7", "8", "9", "+", "-", ".", "0", "="]
-                }/>
-        </main>
+        <div>
+            <h1></h1>
+            <main>
+            
+            </main>
+        </div>
     );
 };
 
