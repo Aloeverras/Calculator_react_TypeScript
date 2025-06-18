b858d860dd80cfb83f5cfa5dffee64551582961d
Aloeverras <edouard.bouche@gmail.com>
2025-06-18 10:41:31 +0000

    button calculator

--- frontend/src/components/ButtonCalculator.tsx
+++ frontend/src/components/ButtonCalculator.tsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react";
 import type { ButtonCalculatorProps } from "../types/ButtonCalculatorProps";
 import type { ButtonCalculatorType } from "../types/ButtonCalculatorType";
 import {Button} from "reactstrap";
 
-const ButtonCalculator : ButtonCalculatorType = (props : ButtonCalculatorProps) => {
+const ButtonCalculator : ButtonCalculatorType = ({label = "", id = "", classNames = "", isMono = false, ...rest} : ButtonCalculatorProps) => {
 
-    
+    label = isMono && label.length > 1 ? label.charAt(0) : label;
+
+    const [labelHook, labelHookSet] = useState<string>(label);
+
+    useEffect(() => {
+        labelHookSet(label),
+        [
+            label,
+            isMono
+        ]
+    })
+
+    const HANDCLICK : () => void = () => {
+        if (rest.onClick) {
+            rest.onClick()
+        } else {
+            console.error("handclic error")
+        }
+    }
 
     return (
-        <Button id={props.id} className={props.classNames}>
-            {
-                props.label
-            }
+        <Button onClick={HANDCLICK}
+        id={id}
+        className={classNames}
+        {...rest}>    
+            {labelHook}
         </Button>
     );
 }
