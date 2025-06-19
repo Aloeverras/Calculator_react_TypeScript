90a1dd663e30df4af46b59b9a8a3ce28aa9ec923
Aloeverras <edouard.bouche@gmail.com>
2025-06-19 07:34:41 +0000

    2025_06_19

--- frontend/src/types/ButtonCalculatorType.ts
+++ frontend/src/types/ButtonCalculatorType.ts
@@ -1,4 +1,4 @@
 import type React from "react";
 import type { ButtonCalculatorProps } from "./ButtonCalculatorProps";
 
-export type ButtonCalculatorType = React.FC<ButtonCalculatorProps>
+export type ButtonCalculatorType = React.FC<ButtonCalculatorProps>;
