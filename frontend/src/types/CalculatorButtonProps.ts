8378f8544da9db1fe5d3c10ef0998a68e11082b7
Aloeverras <edouard.bouche@gmail.com>
2025-06-25 08:46:28 +0000

    son type button

--- frontend/src/types/CalculatorButtonProps.ts
+++ frontend/src/types/CalculatorButtonProps.ts
@@ -1,6 +1,7 @@
 import React, { type HTMLAttributes } from "react";
 
 export type CalculatorButtonProps<T> = React.ComponentProps<
-    React.JSXElementConstructor<{label : T} & HTMLAttributes<HTMLButtonElement>
-        >
+    React.JSXElementConstructor<
+            {label : T} & HTMLAttributes<HTMLButtonElement>
+        >  
     >
