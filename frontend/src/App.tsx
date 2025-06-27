52f42e79b9c89b073cc51023d4ee79c09eb614bd
Aloeverras <edouard.bouche@gmail.com>
2025-06-27 09:10:25 +0000

    new board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,28 @@ const App : () => JSX.Element = () => {
   return (
     <>
       <h1>Hello world</h1>
-      <CalBtnComponent label={5}></CalBtnComponent>
+      {
+        ["+", "-", "/", "*"].map((btn : string) => {
+          let typeBtn : string = "";
+          switch(btn){
+            case "+":
+              typeBtn = "plus";
+                break;
+            case "-":
+              typeBtn = "moins";
+                break;
+            case "/":
+              typeBtn = "division";
+                break;
+            case "*":
+              typeBtn = "multiply";
+                break;   
+          }
+          return (
+            <CalBtnComponent label={btn} type="operator" data-testid={`button-${typeBtn}`} onClick={() => console.log(btn)}/>
+          )
+        })
+      }
     </>
   );
 }
